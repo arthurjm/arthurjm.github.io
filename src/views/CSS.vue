cf4c70599ac99f3370c62cd0645cd35fe9856585
Arthur <arthur.mondon@gmail.com>
2022-04-12 11:01:53 +0200

    Add css bind simple demo

--- src/views/CSS.vue
+++ src/views/CSS.vue
@@ -1,7 +1,38 @@
 <template>
   <div id="main">
-    <div id="red"></div>
-    <div id="green"></div>
+    <div id="actions" class="box">
+      <label
+        >Couleur<button @click="changeColor">
+          <font-awesome-icon icon="dice" size="2x" />
+        </button>
+        <input type="range" name="hue" min="0" max="360" v-model="hue" />
+      </label>
+      <label
+        >Largeur<input
+          type="range"
+          name="width"
+          min="100"
+          max="500"
+          v-model="widthValue"
+      /></label>
+      <label
+        >Hauteur<input
+          type="range"
+          name="height"
+          min="100"
+          max="500"
+          v-model="heightValue"
+      /></label>
+      <label
+        >Rotation<input
+          type="range"
+          name="height"
+          min="0"
+          max="360"
+          v-model="angle"
+      /></label>
+    </div>
+    <div class="rectangle"></div>
   </div>
 </template>
 
@@ -10,30 +41,56 @@ export default {
   name: "css-demo",
   components: {},
   data: function () {
-    return {};
+    return {
+      hue: 0,
+      widthValue: 250,
+      heightValue: 250,
+      angle: 0,
+    };
   },
-  methods: {},
+  methods: {
+    changeColor() {
+      this.hue = Math.floor(Math.random() * 360);
+    },
+  },
+  computed: {
+    color() {
+      return `hsl(${this.hue}, 100%, 50%)`;
+    },
+    width() {
+      return `${this.widthValue}px`;
+    },
+    height() {
+      return `${this.heightValue}px`;
+    },
+    rotate() {
+      return `rotate(${this.angle}deg)`;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-$red: red;
-$green: green;
-
-#main {
-  width: 500px;
-  height: 500px;
-}
-
-#red {
-  background-color: $red;
-  height: 100px;
-  height: 200px;
-}
-
-#green {
-  background-color: $green;
+#actions {
+  z-index: 1;
+  position: relative;
   width: 300px;
-  height: 450px;
+}
+
+label {
+  z-index: 1;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.rectangle {
+  z-index: 0;
+  margin: 1% auto;
+  width: v-bind(width);
+  height: v-bind(height);
+  background-color: v-bind(color);
+  transform: v-bind(rotate);
 }
 </style>
