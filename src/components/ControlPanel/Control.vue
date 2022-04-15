25faa62aed759a22ff26efc5a05549aee92e2882
Arthur <arthur.mondon@gmail.com>
2022-04-15 13:21:02 +0200

    Split control panel in multiple components

---
+++ src/components/ControlPanel/Control.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    {{ data.name }} ({{ data.value }}{{ data.unit }})
+    <InputRange
+      v-if="data.type === 'InputRange'"
+      :data="data"
+      @update:data="update"
+    />
+  </div>
+</template>
+
+<script>
+import InputRange from "@/components/ControlPanel/InputRange.vue";
+
+export default {
+  name: "control-panel-control",
+
+  components: {
+    InputRange,
+  },
+
+  props: ["data"],
+
+  emits: ["update:data"],
+
+  methods: {
+    update(d) {
+      let data = this.data;
+      data.value = d;
+      this.$emit("update:data", data);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
